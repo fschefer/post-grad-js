366c3fec394a66791f00b788f32d5d3e1a614a92
Flávio Schefer <flavio.schefer@gmail.com>
2024-04-05 21:55:50 -0300

    First commit of the first class

---
+++ a1.js
@@ -0,0 +1,13 @@
+let name;
+name = "Flávio"
+console.log("Happy testing, " + name)
+
+/* 
+    Block comment here!
+    */
+
+    //Normal comment here...
+
+let love = "Arabella"
+
+console.log(name + " loves " + love)
